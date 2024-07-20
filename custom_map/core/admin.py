23d58285764aadd9367c3ce8682331ab8fdb0f7c
Nishant7620 <nkaranjavkar72@gmail.com>
2024-07-20 23:12:08 +0530

    models are created

--- custom_map/core/admin.py
+++ custom_map/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Location
 
 # Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id','name1','name2','price','satta_bara_no']
--- custom_map/core/models.py
+++ custom_map/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+
+    name1 = models.CharField(max_length=100)
+    name2 = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    satta_bara_no = models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.name1
+    
